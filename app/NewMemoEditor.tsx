7e9d30b25e3219ec5c13dfe308d55b47b5210951
bhwa233 <404174262@qq.com>
2025-03-20 15:00:35 +0800

    feat: 移除创建新笔记时的标签更新逻辑，简化成功回调处理

--- app/NewMemoEditor.tsx
+++ app/NewMemoEditor.tsx
@@ -2,25 +2,23 @@
 import React from 'react';
 import Editor from '@/components/Editor';
 import { useRequest } from 'ahooks';
-import { createNewMemo, regenerateMemeTags } from '../src/api/dbActions';
+import { createNewMemo } from '../src/api/dbActions';
 import useMemoStore from '../src/store/memo';
 import { useToast } from '../src/components/ui/use-toast';
 import { startConfettiAnimation } from '../src/lib/utils';
 
 const NewMemoEditor: React.FC = () => {
-    const { fetchFirstData, updateMemo } = useMemoStore();
+    const { fetchFirstData } = useMemoStore();
     const { toast } = useToast();
     const { runAsync: createRecord } = useRequest(createNewMemo, {
         manual: true,
-        onSuccess: async ({id}) => {
+        onSuccess: async () => {
             toast({
                 title: '创建成功',
                 description: '已成功创建新笔记',
             });
             startConfettiAnimation();
             fetchFirstData()
-            await regenerateMemeTags(id);
-            updateMemo(id);
         }
     })
     return (

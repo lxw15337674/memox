04b45a7c9bcb503240841c3891e326d5c49dff84
bhwa233 <404174262@qq.com>
2025-08-06 18:03:50 +0800

    fix(TagsSection): 添加高度限制以改善标签区域的显示效果

--- src/components/Tools/TagsSection/index.tsx
+++ src/components/Tools/TagsSection/index.tsx
@@ -9,7 +9,7 @@ const TagsSection = () => {
     return null;
   }
   return (
-    <ScrollArea className=" py-2 px-3 bg-card rounded-lg border  text-card-foreground shadow-sm w-full">
+    <ScrollArea className=" py-2 px-3 bg-card rounded-lg h-[50vh] border text-card-foreground shadow-sm w-full">
       <div className="flex flex-row justify-start items-center w-full">
         <span className="text-sm leading-6 font-mono text-gray-400 select-none">
           标签
